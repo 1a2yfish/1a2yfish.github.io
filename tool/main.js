0dcbc96ef246a50d9720d72649370a2b6ea6b05f
Heartsounds <lms0024@nuaa.edu.cn>
2023-09-04 22:03:41 +0800

    Site updated: 2023-09-04 22:03:41

---
+++ tool/main.js
@@ -0,0 +1,9 @@
+function openModal() {
+    var modalOverlay = document.getElementById("modal-overlay");
+    modalOverlay.style.display = "flex";
+  }
+
+  function closeModal() {
+    var modalOverlay = document.getElementById("modal-overlay");
+    modalOverlay.style.display = "none";
+  }
