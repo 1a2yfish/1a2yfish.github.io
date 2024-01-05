84911ce3efb26784867439ef2916a4292d7dd1d8
1azy_fish <3500542609@qq.com>
2024-01-06 00:06:01 +0800

    Site updated: 2024-01-06 00:05:59

--- tool/main.js
+++
@@ -1,9 +0,0 @@
-function openModal() {
-    var modalOverlay = document.getElementById("modal-overlay");
-    modalOverlay.style.display = "flex";
-  }
-
-  function closeModal() {
-    var modalOverlay = document.getElementById("modal-overlay");
-    modalOverlay.style.display = "none";
-  }
